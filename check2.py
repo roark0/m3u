9152003fe9061c4a1f0ce99a88227086c1a3fba5
lordyung <lordyung@duck.com>
2023-11-27 18:22:23 +0800

    update check2.py
    Signed-off-by: lordyung <lordyung@duck.com>

--- check2.py
+++ check2.py
@@ -18,56 +18,67 @@
 2.按照"api"字段排序父节点, 如果api字段相同,则删除重复api对应字段的父节点
 3.检查所有api包含的链接是否可访问,如果不能访问,则删除它包含的父节点,
 4.并做相应的异常处理包括SSLError, RequestException
-5.要求一次处理5个节点,并打印处理进度
+5.要求同时处理10个节点,并打印处理进度
 6.添加对应的段注释
 请帮忙实现
 """
+import json
 import requests
 from requests.exceptions import SSLError, RequestException
-import json
+from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor
 
-def check_api_availability(api):
+def check_api_availability(url):
     try:
-        response = requests.get(api, timeout=5)
-        # return response.status_code == 200
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
         return True
     except (SSLError, RequestException):
         return False
 
-def process_nodes(nodes):
-    sorted_nodes = sorted(nodes, key=lambda x: x['api'])  # 按照"api"字段排序父节点
-    unique_apis = []
+def process_node(node):
+    api_url = node['api']
+    if not check_api_availability(api_url):
+        return None
+    return node
+
+def process_parent_nodes(parent_nodes):
     processed_nodes = []
-
-    for node in sorted_nodes:
-        if node['api'] not in unique_apis:  # 如果api字段不重复
-            unique_apis.append(node['api'])
-            if check_api_availability(node['api']):  # 检查api是否可访问
-                processed_nodes.append(node)
-                print("已处理节点:", node)
-            else:
-                print("无法访问的API链接:", node['api'])
-        else:
-            print("重复的API链接，已删除节点:", node)
-
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        for node in parent_nodes:
+            futures.append(executor.submit(process_node, node))
+        for future in futures:
+            if future.result():
+                processed_nodes.append(future.result())
+            print("Processed", len(processed_nodes), "out of", len(parent_nodes), "nodes")
     return processed_nodes
 
-def process_file(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        json_data = data['json']
-        total_nodes = len(json_data)
-        processed_data = []
-        for i in range(0, total_nodes, 5):
-            batch_nodes = json_data[i:i+5]
-            processed_nodes = process_nodes(batch_nodes)
-            processed_data.extend(processed_nodes)
-            print("进度: {}/{}".format(i + len(batch_nodes), total_nodes))
-        
-        data['json'] = processed_data
+def sort_and_remove_duplicates(json_data):
+    sorted_nodes = sorted(json_data['json'], key=lambda x: x['api'])
+    unique_nodes = []
+    prev_api = None
+    for node in sorted_nodes:
+        if node['api'] != prev_api:
+            unique_nodes.append(node)
+        prev_api = node['api']
+    return unique_nodes
 
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+def main():
+    # 读取并解析site2.json文件
+    with open('site2.json', 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
 
-# 调用函数处理site2.json文件
-process_file('site2.json')
+    # 按照api字段排序并删除重复的父节点
+    unique_nodes = sort_and_remove_duplicates(json_data)
+
+    # 处理父节点并检查api链接的可访问性
+    processed_nodes = process_parent_nodes(unique_nodes)
+
+    # 更新json_data并保存到新的json文件
+    json_data['json'] = processed_nodes
+    with open('processed_site2.json', 'w', encoding='utf-8') as file:
+        json.dump(json_data, file, ensure_ascii=False, indent=4)
+
+if __name__ == '__main__':
+    main()
