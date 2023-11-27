726a571d7027016faaecc6d1ba9bc9572be40533
lordyung <lordyung@duck.com>
2023-11-27 14:36:01 +0800

    filter movie.m3u
    Signed-off-by: lordyung <lordyung@duck.com>

--- m3up.py
+++ m3up.py
@@ -7,7 +7,7 @@ import sys
 
 from m3u_parser import M3uParser
 
-def parse_and_filter_m3u(url):
+def parse_and_filter_m3u(url, filter='GOOD', sort='name'):
     useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 
     # Instantiate the parser
@@ -19,19 +19,24 @@ def parse_and_filter_m3u(url):
     # Remove by mp4 extension
     # parser.remove_by_extension('mp4')
 
-    # Filter streams by status
-    # parser.filter_by('status', 'GOOD')
-
-    parser.sort_by("url", key_splitter="-", asc=True, nested_key=False)
+    parser.sort_by(sort, key_splitter="-", asc=True, nested_key=False)
 
     parser.remove_duplicates()
+
+    # parser.filter_by('status', filter)
 
     # Get the list of streams
     streams = parser.get_list()
     print(len(streams))
 
-    # Convert streams to JSON and save to a file
-    parser.to_file(url, 'm3u')
+    # base_name = os.path.basename(url)
+    # file_name_without_ext = os.path.splitext(base_name)[0]
+    # new_name = f"{file_name_without_ext}_{filter}.m3u"
+    # print(new_name)
+   
+    # Convert bad streams to m3u and save to a file
+    if (len(streams)):
+        parser.to_file(url, 'm3u')
 
 # 检查命令行参数是否包含URL
 if len(sys.argv) < 2:
@@ -44,3 +49,4 @@ url = sys.argv[1]
 # 在这里使用获取到的URL参数进行后续操作
 print("传递的URL参数是:", url)
 parse_and_filter_m3u(url)
+
